43def90a5bf7c6466b54806b2c4fda12b2fa4bdb
Niki Fereidooni <nikifereidooni@gmail.com>
2020-10-16 12:08:21 -0400

    Added process env PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const saveFile = './.tableList.json' // save file, it's a hidden file
 
 // will share any static html files with the browser
